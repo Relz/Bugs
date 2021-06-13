2ec68d4b0def8fe7939b5d44c4b2aa7cf1b855ce
Relz <relz0071@gmail.com>
2021-06-13 20:25:27 +0300

    Исправить название поля для сохранения кол-ва очков

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -250,7 +250,7 @@ public class GameController : MonoBehaviourPunCallbacks
                 {
                     Dictionary<string, object> score = new Dictionary<string, object>
                 {
-                    { "score", GetScore() },
+                    { "value", GetScore() },
                     { "nickName", player.NickName },
                     { "gameId", "Bugs" },
                     { "gameSessionId", gameSessionId },
